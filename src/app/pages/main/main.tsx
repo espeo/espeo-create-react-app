ad4222d4d87e5a2f088da70028be3312d49896cc
Tomasz Agaciński <>
2019-11-18 15:05:43 +0100

    uncommented code

--- src/app/pages/main/main.tsx
+++ src/app/pages/main/main.tsx
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import { InjectedIntlProps } from 'react-intl';
-// import { v1 } from 'uuid';
+import { v1 } from 'uuid';
 import { Input, Button } from '@core/components';
 import { loadItems } from './store/actions';
 
@@ -36,8 +36,7 @@ class MainComponent extends PureComponent<MainComponentProps> {
   };
 
   render() {
-    // const { items, intl } = this.props;
-    const { intl } = this.props;
+    const { items, intl } = this.props;
 
     return (
       <div>
@@ -48,11 +47,11 @@ class MainComponent extends PureComponent<MainComponentProps> {
           text="example button"
           clickHandler={this.handleClick}
         />
-        {/* <ul>
+        <ul>
           {items.map((item: any) => {
             return <li key={v1()}>{item.name}</li>;
           })}
-        </ul> */}
+        </ul>
       </div>
     );
   }
