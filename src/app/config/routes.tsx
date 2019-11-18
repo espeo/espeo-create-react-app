1a9d07ba28f4066261369ed5a1a6cee347708afb
Tomasz Agaciński <>
2019-11-18 15:46:44 +0100

    added dynamic route as separate bundle

--- src/app/config/routes.tsx
+++ src/app/config/routes.tsx
@@ -2,13 +2,14 @@ import React, { ComponentClass } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import MainPage from '@core/pages/main';
-import MyDynamicRoute from '@core/pages/dynamic-route/dynamic-route';
+
+const MyDynamicRoute = React.lazy(() => import(/* webpackChunkName: "dynamic-route" */ '@core/pages/dynamic-route/dynamic-route'));
 
 export const AppRoutes = () => (
   <Router>
     <Switch>
       <Route path="/" exact component={MainPage as ComponentClass} />
-        <Route exact path="/dynamic-route" component={MyDynamicRoute as any} />
+      <Route exact path="/dynamic-route" component={MyDynamicRoute as any} />
     </Switch>
   </Router>
 );
