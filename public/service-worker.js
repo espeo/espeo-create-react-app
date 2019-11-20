1b82d4a774a3582f949e51cf2f6ec73c77649d18
Tomasz Agaciński <>
2019-11-20 11:19:51 +0100

    added https flag to start:mocked script

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,38 +11,42 @@ const staticCacheName = 'static-cache-v1';
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(staticCacheName)
-        .then(cache => {
-            return cache.addAll(filesToCache);
-        })
+            .then(cache => {
+                return cache.addAll(filesToCache);
+            })
     );
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
         fetch(event.request)
-        .then(response => {
-            if (response.status === 404) {
-                return caches.match('./404.html');
-            }
-            if (response.status === 200) {
-                console.log('jest net');
-                
-                return caches.open(staticCacheName)
-                    .then(cache => {
-                        cache.put(event.request, response.clone());
-                        return response;
-                    });
-            }
+            .then(response => {
+                if (response.status === 404) {
+                    return caches.match('./404.html');
+                }
+                if (response.status === 200) {
+                    console.log('jest net');
 
-        }).catch(error => {
-            return caches.match(event.request)
-                .then(response => {
-                    if (response) {
-                        return response;
-                    }
-                    return caches.match('./offline.html');
-                })
-        })
+                    return caches.open(staticCacheName)
+                        .then(cache => {
+                            console.log('kesz', cache);
+
+                            cache.put(event.request, response.clone());
+
+                            return response;
+                        });
+                }
+
+            }).catch(error => {
+                return caches.match(event.request)
+                    .then(response => {
+                        if (response) {
+                            return response;
+                        }
+                        console.error(error)''
+                        return caches.match('./offline.html');
+                    })
+            })
     )
 });
 
