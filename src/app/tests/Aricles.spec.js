dc6bea3b7349808dfe64eb172d5931d3d259d266
Adam Kosmala <adam.kosmala@espeo.eu>
2019-11-27 13:58:37 +0100

    fixed config of unit tests

--- src/app/tests/Aricles.spec.js
+++ src/app/tests/Aricles.spec.js
@@ -1,12 +1,12 @@
-//import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer';
 
-// import MainArticles from './components/MainArticles/MainArticles.component';
+import MainArticles from '../pages/MainArticles/components/MainArticles/MainArticles.component';
 
 describe('Articles component test suite', () => {
   it('snapshot renders', () => {
-    // const component = renderer.create(<MainArticles />);
-    // let tree = component.toJSON();
-    // expect(tree).toMatchSnapshot();
+    const component = renderer.create(<MainArticles />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
     expect(true).toBe(true);
   });
 });
